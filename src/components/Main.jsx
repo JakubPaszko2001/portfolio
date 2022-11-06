a7abbe2810db5bd256594d7a05586424a7289d38
JakubPaszko2001 <jakubpaszko01@gmail.com>
2022-11-06 12:50:42 +0100

    hamburger

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect } from "react";
 import bg from "../Image/bg.jpg";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { CgClose } from "react-icons/cg";
 import SplitType from "split-type";
 import gsap from "gsap";
 const Main = () => {
@@ -9,12 +10,14 @@ const Main = () => {
   useEffect(() => {
     const nameSplit = new SplitType("#name");
     const jobSplit = new SplitType("#job");
-    gsap.to(".char", {
-      y: 0,
-      stagger: 0.05,
-      delay: 0.2,
-      duration: 0.4,
-    });
+    const tl = gsap.timeline();
+    const hamburgerTl = gsap.timeline();
+
+    tl.to(".char", { y: 0, stagger: 0.05, delay: 0.2, duration: 0.4 }).fromTo(
+      ".hamburger",
+      { y: "20", opacity: 0 },
+      { y: 0, opacity: 1, duration: 1 }
+    );
   });
 
   return (
@@ -33,8 +36,19 @@ const Main = () => {
           Frontend Developer
         </h1>
       </div>
-      <div className="absolute bottom-10 right-10 text-5xl">
+      <div className="hamburger absolute bottom-10 right-10 text-5xl">
         <GiHamburgerMenu />
+      </div>
+      <div className="absolute top-0 right-0 h-full w-1/2 flex justify-center items-center bg-[dimgray]">
+        <div className="h-1/3 flex flex-col justify-between text-3xl">
+          <h1>About</h1>
+          <h1>Skills</h1>
+          <h1>Projects</h1>
+          <h1>Contact</h1>
+        </div>
+        <div className="absolute bottom-10 right-10 text-5xl">
+          <CgClose />
+        </div>
       </div>
     </div>
   );

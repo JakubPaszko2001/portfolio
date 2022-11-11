d8324dd2ab87ede51d139385812b0fb704a373ef
JakubPaszko2001 <jakubpaszko01@gmail.com>
2022-11-11 15:07:21 +0100

    blad gsap

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,36 +1,30 @@
 import React, { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-
 gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
-  const x = useRef(null);
   useEffect(() => {
-    const el = x.current;
-    gsap.fromTo(
-      el,
-      { x: 0 },
-      {
-        x: 300,
-        duration: 1,
-        scrollTrigger: {
-          trigger: el,
-          pin: true,
-          markers: true,
-        },
-      }
-    );
+    gsap.to(".text", {
+      scrollTrigger: {
+        trigger: ".first",
+        markers: true,
+        start: "top top",
+        end: "+=400",
+        pin: ".first",
+        scrub: 2,
+      },
+      opacity: 1,
+      y: 200,
+    });
   });
+
   return (
     <div>
-      <div className="abu relative h-screen w-screen bg-light dark:bg-dark">
-        <div
-          ref={x}
-          className="box w-80 h-80 absolute top-0 left-0 bg-yellow-500"
-        ></div>
+      <div className="first flex justify-center items-center h-screen w-screen bg-light dark:bg-dark">
+        <h1 className="text">Hello Hello</h1>
       </div>
-      <div className="h-screen w-screen bg-black"></div>
+      <div className="second h-screen w-screen bg-black"></div>
     </div>
   );
 };

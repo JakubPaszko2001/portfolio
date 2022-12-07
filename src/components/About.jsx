17103a09e94b9aef97c3654b2f128d1bd2347a1c
JakubPaszko2001 <jakubpaszko01@gmail.com>
2022-12-07 20:45:05 +0100

    walka z gsap matchMedia

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import zdjecie from "../Image/zdjecie.jpg";
-import pillars from "../Image/pillars.webp";
 import column from "../Image/column.webp";
 gsap.registerPlugin(ScrollTrigger);
 
